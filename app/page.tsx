f2437bddca12d2458830a4de814865ebc598ddcf
ionutpetru4046 <ionut4046@yahoo.com>
2026-06-14 20:37:01 +0100

    components added into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Pricing from "@/components/Pricing";
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>TradeFlow</h1>
-    </main>
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+    </>
   );
 }
